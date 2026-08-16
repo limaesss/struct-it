14e6c27df7a41842c0334ed8052054c823326252
limaes <limaess@outlook.com>
2026-08-16 17:41:14 +0000

    add necessary functions; change dynamic_array

--- include/dynamic_array.h
+++ include/dynamic_array.h
@@ -5,9 +5,23 @@
 #include <stdbool.h>
 
 typedef struct {
-    void** array;
-    size_t size;
+    void* data;
+    size_t element_size; // size of each element
     size_t count;
+    size_t capacity;
 } dynamic_array;
+
+void** da_init(dynamic_array* array, size_t element_size);
+
+void da_rollback(dynamic_array* array);
+
+void da_append(dynamic_array* array, void* element);
+void da_insert(dynamic_array* array, void* element, int index);
+
+void da_remove(dynamic_array* array, int index);
+
+void* da_get(dynamic_array* array, int index);
+
+void da_free(dynamic_array* array);
 
 #endif
